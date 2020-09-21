81599ecda0181fafe325ce2c1303cb54b157063d
EasyBB1 <easybb.gen@gmail.com>
2020-09-21 12:23:02 +1000

    Update win-dscp-ping.bat

--- win-dscp-ping.bat
+++ win-dscp-ping.bat
@@ -1,12 +1,14 @@
 REM Windows script to test DSCP markings
 @ECHO OFF
 REM Enter your SIP server address below
-SET SERVER="google.com.au"
+SET SERVER=google.com.au
+SET FILE=dscp.txt
 SETLOCAL EnableDelayedExpansion
-ECHO. > dscp.txt
+ECHO. > %FILE%
+ECHO Results for %SERVER% >> %FILE%
 FOR /L %%A IN (0, 1, 63) DO (
 SET /A tos = %%A * 4
-ECHO DSCP %%A ToS !tos! >> dscp.txt
+ECHO DSCP %%A ToS !tos! >> %FILE%
 PING -n 1 -v !tos! %SERVER% | FIND "TTL="
-IF ERRORLEVEL 1 ( ECHO Failed  >> dscp.txt ) ELSE ( ECHO Passed  >> dscp.txt )
-ECHO. >> dscp.txt)
+IF ERRORLEVEL 1 ( ECHO Failed  >> dscp.txt ) ELSE ( ECHO Passed  >> %FILE% )
+ECHO. >> %FILE%)
