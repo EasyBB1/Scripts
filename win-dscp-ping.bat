08832209e29748f76737fb9a9408d7ec4179c008
EasyBB1 <easybb.gen@gmail.com>
2020-09-21 12:37:05 +1000

    Update win-dscp-ping.bat

--- win-dscp-ping.bat
+++ win-dscp-ping.bat
@@ -5,10 +5,11 @@ SET SERVER=google.com.au
 SET FILE=dscp.txt
 SETLOCAL EnableDelayedExpansion
 ECHO. > %FILE%
-ECHO Results for %SERVER% >> %FILE%
+ECHO Results for %SERVER% & ECHO Results for %SERVER% >> %FILE%
 FOR /L %%A IN (0, 1, 63) DO (
 SET /A tos = %%A * 4
-ECHO DSCP %%A ToS !tos! >> %FILE%
+ECHO DSCP %%A ToS !tos! & ECHO DSCP %%A ToS !tos! >> %FILE%
 PING -n 1 -v !tos! %SERVER% | FIND "TTL="
-IF ERRORLEVEL 1 ( ECHO Failed  >> %FILE% ) ELSE ( ECHO Passed  >> %FILE% )
-ECHO. >> %FILE%)
+IF ERRORLEVEL 1 ( ECHO Failed  ECHO Failed  >> %FILE% ) ELSE ( ECHO Passed & ECHO Passed >> %FILE% )
+ECHO. & ECHO. >> %FILE%)
+PAUSE
